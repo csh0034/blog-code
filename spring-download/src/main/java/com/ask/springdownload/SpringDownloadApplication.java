e7bfecd454ad0a7866218037a5793428e6f9b40c
csh0034 <csh0034@gmail.com>
2022-01-01 17:15:37 +0900

    feat(spring-download): init

---
+++ spring-download/src/main/java/com/ask/springdownload/SpringDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.ask.springdownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDownloadApplication.class, args);
+	}
+
+}
