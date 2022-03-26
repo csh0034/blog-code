cde41392aef6c0919465c315635e02a9ac7ee30d
csh0034 <csh0034@gmail.com>
2022-03-26 19:00:57 +0900

    feat(spring-jpa-set): directory 변경

--- spring-jpa-set/src/main/resources/sql/data.sql
+++
@@ -1,5 +0,0 @@
-insert into tb_company values('company01', 'cname');
-
-insert into tb_user values ('user21', 'user21', 3, 'company01');
-insert into tb_user values ('user22', 'user22', 2, 'company01');
-insert into tb_user values ('user23', 'user23', 1, 'company01');
