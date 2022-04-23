e5c6f54e7ecaf2b6583cd30d3c65ac992deeec47
csh0034 <csh0034@gmail.com>
2022-04-23 22:45:38 +0900

    feat(spring-security-setting): init

---
+++ spring-security/spring-security-setting/src/main/java/com/ask/security/SpringSecuritySettingApplication.java
@@ -0,0 +1,13 @@
+package com.ask.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySettingApplication.class, args);
+	}
+
+}
---
+++ spring-security/spring-security-setting/src/test/java/com/ask/security/SpringSecuritySettingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ask.security;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class SpringSecuritySettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
