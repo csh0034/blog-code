2aa1714df976b618e724dd65effbfd01b91dd730
csh0034 <csh0034@gmail.com>
2022-04-29 22:16:07 +0900

    feat(spring-security-authorization): init

---
+++ spring-security/spring-security-authorization/src/main/java/com/ask/authorization/SpringSecurityAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.ask.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthorizationApplication.class, args);
+	}
+
+}
